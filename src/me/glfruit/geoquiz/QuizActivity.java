f6acf40d6e5571c934b3b518af37b019a5c603ab
Fruit Lee <glfruit80@gmail.com>
2014-03-07 15:19:02 +0800

    添加调试信息

--- src/me/glfruit/geoquiz/QuizActivity.java
+++ src/me/glfruit/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package me.glfruit.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -30,9 +31,42 @@ public class QuizActivity extends Activity {
 
 	private int mCurrentIndex = 0;
 
+	private static final String TAG = "QuizActivity";
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		Log.d(TAG, "onStart() called");
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		Log.d(TAG, "onPause() called");
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.d(TAG, "onResume() called");
+	}
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		Log.d(TAG, "onStop() called");
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.d(TAG, "onDestroy() called");
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.d(TAG, "onCreated(Bundle) called");
 		setContentView(R.layout.activity_quizi);
 
 		mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
